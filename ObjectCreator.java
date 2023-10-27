81c561a358bfd7657d4305f105f233567e3ae5d0
Dawood Afzaal <dawoodafzaal13@gmail.com>
2023-10-27 17:55:16 -0600

    created object creator which now creates a simple object based on user input

---
+++ ObjectCreator.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ObjectCreator {
+
+    static class SimpleObject {
+        int intValue;
+        double doubleValue;
+        char charValue;
+        
+        @Override
+        public String toString() {
+            return "SimpleObject { " +
+                   "intValue=" + intValue +
+                   ", doubleValue=" + doubleValue +
+                   ", charValue=" + charValue +
+                   " }";
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int option = 0;
+        Map<String, Integer> map = Map.of("one", 1, "two", 2, "three", 3);
+        System.out.println("Create a type of Object. \n");
+
+        while(option != 3) {
+            System.out.println("---- Object Creator Menu ----");
+            System.out.println("1. Simple Object \n" + "2. An object that contains an array of primitives. \n" + "3. Exit \n");
+            option = scanner.nextInt();
+
+            if(!(map.containsValue(option))){
+                System.out.println("Invalid choice, exiting program.");
+                scanner.close();
+                System.exit(1);
+            }
+            else if(option == 1) {
+                SimpleObject simpleObj = new SimpleObject();
+
+                System.out.print("Enter int value for int field in object: ");
+                simpleObj.intValue = scanner.nextInt();
+                
+                System.out.print("Enter double value for double field in object: ");
+                simpleObj.doubleValue = scanner.nextDouble();
+                
+                System.out.print("Enter char value for char value in object: ");
+                simpleObj.charValue = scanner.next().charAt(0);
+                
+                System.out.println("Created Object: " + simpleObj);
+
+            }
+            else if(option == 3){
+                System.out.println("Exiting Program.");
+            }
+
+            System.out.println("You Entered: " + option);
+        }
+
+        scanner.close();
+        System.exit(0);
+
+        
+
+        
+    }
+}
