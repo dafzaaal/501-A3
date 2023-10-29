03848ad1de9608816a3a839cfe95b4ff53a584d0
Dawood Afzaal <dawoodafzaal13@gmail.com>
2023-10-28 20:26:35 -0600

    added print map helper method

--- ObjectCreator.java
+++ ObjectCreator.java
@@ -1,15 +1,24 @@
 import java.io.*;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 
 public class ObjectCreator {
 
+    public static void printMap(Map<?, ?> map) {
+        for (Map.Entry<?, ?> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         int option = 0;
-        Map<String, Integer> map = Map.of("one", 1, "two", 2, "three", 3);
+        Map<String, Integer> map = Map.of("one", 1, "two", 2, "three", 3, "four", 4);
+        Map<Integer, SimpleObject> createdObjects = new IdentityHashMap<>();
         System.out.println("Create a type of Object. \n");
 
         while(option != 3) {
@@ -35,8 +44,11 @@ public class ObjectCreator {
                 simpleObj.charValue = scanner.next().charAt(0);
                 
                 System.out.println("Created Object: " + simpleObj);
-                Serializer serializer = new Serializer();
-                serializer.serialize(simpleObj);
+                System.out.println("Adding newly created object to the hashmap");
+
+                createdObjects.put(createdObjects.size() + 1, simpleObj);
+
+                printMap(createdObjects);
 
             }
             else if(option == 3) {
@@ -44,6 +56,8 @@ public class ObjectCreator {
             }
             else if(option == 4){
                 System.out.println("Exiting Program.");
+                scanner.close();
+                System.exit(0);
             }
 
             System.out.println("You Entered: " + option);
